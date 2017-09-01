e84924d774aa8a92e2795299b073e9051ef5d9b7
Okwonks <albertoketch@gmail.com>
2017-09-01 15:37:36 +0300

    Adding the save and equals methods to the abstract animal class.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -6,6 +6,7 @@ import java.util.TimerTask;
 
 public abstract class Animal {
     public String name;
+    public String type;
     public int id;
 
     public String getName() {
@@ -14,6 +15,28 @@ public abstract class Animal {
 
     public int getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName()) &&
+                   this.getId() == newAnimal.getId();
+        }
+    }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, type) VALUES (:name, :type)";
+            this.id = (int) con.createQuery(sql, true)
+              .addParameter("name", this.name)
+              .addParameter("type", this.type)
+              .executeUpdate()
+              .getKey();
+        }
     }
 
 }

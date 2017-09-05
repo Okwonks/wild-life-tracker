0eae05232d2ea3083741c516e3f388c748838a24
Okwonks <albertoketch@gmail.com>
2017-09-05 19:10:26 +0300

    Starting the testing on the SafeAnimal class.

---
+++ src/main/java/SafeAnimal.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class SafeAnimal {
+    public static final String DATABASE_TYPE = "safe";
+    
+}
---
+++ src/test/java/SafeAnimalTest.java
@@ -0,0 +1,62 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+public class SafeAnimalTest {
+    
+        @Rule
+        public DatabaseRule database = new DatabaseRule();
+    
+        @Test
+        public void endangeredAnimal_InstantiatesCorrectly_true() {
+            SafeAnimal testSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            assertTrue(testSafeAnimal instanceof SafeAnimal);
+        }
+    
+        @Test
+        public void SafeAnimal_instantiatesWithName_String() {
+            SafeAnimal testSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            assertEquals("Rhino", testSafeAnimal.getName());
+        }
+    
+        @Test
+        public void equals_returnsTrueIfSafeAnimalDetailsAreEqual() {
+            SafeAnimal testSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            SafeAnimal otherSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            assertTrue(testSafeAnimal.equals(otherSafeAnimal));
+        }
+    
+        @Test
+        public void save_addsSafeAnimalToDatabase_successfully() {
+            SafeAnimal testSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            testSafeAnimal.save();
+            assertTrue(SafeAnimal.all().get(0).equals(testSafeAnimal));
+        }
+    
+        @Test
+        public void save_assignsIdToSafeAnimal() {
+            SafeAnimal testSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            testSafeAnimal.save();
+            SafeAnimal savedEndangeredAmAnimal = SafeAnimal.all().get(0);
+            assertEquals(savedEndangeredAmAnimal.getId(), testSafeAnimal.getId());
+        }
+    
+        @Test
+        public void all_returnsAllInstancesOfSafeAnimals() {
+            SafeAnimal firstSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            firstSafeAnimal.save();
+            SafeAnimal secondSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            secondSafeAnimal.save();
+            assertTrue(SafeAnimal.all().get(0).equals(firstSafeAnimal));
+            assertTrue(SafeAnimal.all().get(1).equals(secondSafeAnimal));
+        }
+    
+        @Test
+        public void find_returnsSafeAnimalWithSameId_secondAnimal() {
+            SafeAnimal firstSafeAnimal = new SafeAnimal("Rhino", "okay", "young");
+            firstSafeAnimal.save();
+            SafeAnimal secondSafeAnimal = new SafeAnimal("Elephant", "okay", "old");
+            secondSafeAnimal.save();
+            assertEquals(SafeAnimal.find(secondSafeAnimal.getId()), secondSafeAnimal);
+        }
+    }

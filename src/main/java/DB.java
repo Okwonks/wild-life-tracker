650edc48cf77e362b335bba79f3e5baa1ba07eac
Okwonks <albertoketch@gmail.com>
2017-09-06 16:45:27 +0300

    Changing the DB.java logic for deployment to herokuapp.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,31 @@
 import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "albert", "2525");
+    private static URI dbUri; 
+    public static Sql2o sql2o;
+
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/hair_salon");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "albert" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "2525" : dbUri.getUserInfo().split(":")[1];
+
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            //add something here
+        }
+    }
+
 }
 

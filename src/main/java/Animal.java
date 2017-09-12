f64b014a9fc1e541f699cbec71bf44f99fddb25d
Okwonks <albertoketch@gmail.com>
2017-09-12 09:09:22 +0300

    Refactoring code. Removing unnecessary imports from the class.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,9 +1,5 @@
 import org.sql2o.*;
 import java.util.List;
-import java.sql.Timestamp;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public abstract class Animal {
     public String name;
     public String type;
